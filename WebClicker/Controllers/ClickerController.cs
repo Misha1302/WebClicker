cb4289e31b0485efdbde3472cc950b4b97bc98ab
Михаил <77919295+Misha1302@users.noreply.github.com>
2023-01-27 11:25:56 +0300

    первая версия

---
+++ WebClicker/Controllers/ClickerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebClicker.Data;
+
+namespace WebClicker.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClickerController
+{
+    private const int StartMoney = 0;
+    private static readonly List<PlayerData> _playersData = new();
+
+    [HttpGet("/users{id:int}/score")]
+    public int GetMoney(int id)
+    {
+        var playerData = _playersData.Find(x => x.Id == id);
+        return playerData?.Money ?? int.MinValue;
+    }
+
+    [HttpGet("newUser")]
+    public bool RegisterNewUser(int id)
+    {
+        var playerData = _playersData.Find(x => x.Id == id);
+        if (playerData != null) return false;
+
+        _playersData.Add(new PlayerData(id, StartMoney));
+        return true;
+    }
+
+    [HttpGet("/users{id:int}/addScore")]
+    public bool AddMoney(int id)
+    {
+        var playerData = _playersData.Find(x => x.Id == id);
+        if (playerData == null) return false;
+
+        playerData.Money++;
+        return true;
+    }
+}
---
+++ WebClicker/Data/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿namespace WebClicker.Data;
+
+public class PlayerData
+{
+    public int Money { get; set; }
+    public int Id { get; set; }
+
+    public PlayerData(int id, int money)
+    {
+        Id = id;
+        Money = money;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}::{Money}";
+    }
+}
